12e90eb9423114ffa7b94468fbb9ae9222a55686
Monster3D <monster3d@mail.ru>
2019-04-30 21:11:45 +0500

    Change state development script

--- development.php
+++ development.php
@@ -1,3 +1,18 @@
 <?php
 
-echo "Hello World";
+include "vendor/autoload.php";
+
+$lat1 = 56.837744;
+$lat2 = 56.834340;
+
+$lng1 = 60.633386;
+$lng2 = 60.622657;
+
+$gitCalculator = new \GisCalculator\GisCalculator();
+
+$pointA = \GisCalculator\GisCalculator::makePoint($lat1, $lng1);
+$pointB = \GisCalculator\GisCalculator::makePoint($lat2, $lng2);
+
+printf(sprintf("Value: %s\n", $gitCalculator->getDistance($pointA, $pointB)));
+
+
---
+++ tests/Unit/Modules/DistanceTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nikolay
+ * Date: 30/04/2019
+ * Time: 21:04
+ */
+
+namespace Tests\Unit\Modules;
+
+
+class DistanceTest
+{
+
+}
