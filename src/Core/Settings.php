a064a5f1bca9ebdf05b319b9e5c78102c10031cb
Monster3D <monster3d@mail.ru>
2019-04-30 20:47:55 +0500

    Added settings container

--- src/Core/Settings.php
+++ src/Core/Settings.php
@@ -1,15 +1,39 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: nikolay
- * Date: 30/04/2019
- * Time: 19:44
- */
 
 namespace GisCalculator\Core;
 
-
-class Settings
+/**
+ * Class Settings
+ * @package GisCalculator\Core
+ */
+class Settings implements SettingsInterface
 {
+    /**
+     * @var array
+     */
+    private $settingContainer;
 
+    /**
+     * @param string $key
+     * @return null|string
+     */
+    public function getValue(string $key): ?string
+    {
+        $result = null;
+
+        if (array_key_exists($key, $this->settingContainer)) {
+            $result = $this->settingContainer[$key];
+        }
+
+        return $result;
+    }
+
+    /**
+     * @param string $key
+     * @param string $value
+     */
+    public function setValue(string $key, string $value)
+    {
+        $this->settingContainer[$key] = $value;
+    }
 }
