9a2185852294b93bbaeb2204f4590424848be2ed
Monster3D <monster3d@mail.ru>
2019-04-30 20:48:22 +0500

    Added new Point element

--- src/Element/Point.php
+++ src/Element/Point.php
@@ -1,15 +1,48 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: nikolay
- * Date: 30/04/2019
- * Time: 19:13
- */
 
 namespace GisCalculator\Element;
 
-
+/**
+ * Class Point
+ * @package GisCalculator\Element
+ */
 class Point
 {
+    /**
+     * @var float
+     */
+    private $latitude;
 
+    /**
+     * @var float
+     */
+    private $longitude;
+
+    /**
+     * Point constructor.
+     * @param float $latitude
+     * @param float $longitude
+     */
+    public function __construct(float $latitude, float $longitude)
+    {
+        $this->latitude  = $latitude;
+        $this->longitude = $longitude;
+
+    }
+
+    /**
+     * @return float
+     */
+    public function getLatitude() : float
+    {
+        return $this->latitude;
+    }
+
+    /**
+     * @return float
+     */
+    public function getLongitude() : float
+    {
+        return $this->longitude;
+    }
 }
