76a25518648153d7d47cb11e3ae0781f5f13743d
Monster3D <monster3d@mail.ru>
2019-05-01 21:12:02 +0500

    Added new colletion

---
+++ src/Element/CollectionPoints.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace GisCalculator\Element;
+
+use Generator;
+
+/**
+ * Class CollectionPoints
+ * @package GisCalculator\Element
+ */
+class CollectionPoints
+{
+    /**
+     * @var Point[]
+     */
+    private $points = [];
+
+    /**
+     * @param Point $point
+     * @return CollectionPoints
+     */
+    public function setPoint(Point $point) : self
+    {
+        $this->points[] = $point;
+
+        return $this;
+    }
+
+    /**
+     * @param float $latitude
+     * @param float $longitude
+     * @return CollectionPoints
+     */
+    public function setCoordinate(float $latitude, float $longitude) : self
+    {
+        $point = new Point($latitude, $longitude);
+        $this->points[] = $point;
+
+        return $this;
+    }
+
+    /**
+     * @param array $coordinate
+     * @return CollectionPoints
+     */
+    public function setArrayCoordinate(array $coordinate) : self
+    {
+        foreach ($coordinate as $arrayPoint) {
+            $point = new Point($arrayPoint[0], $arrayPoint[1]);
+            $this->points[] = $point;
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Generator
+     */
+    public function getIterator() : Generator
+    {
+        yield from $this->points;
+    }
+}
