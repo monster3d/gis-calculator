34a01b79158744053d441fd67579f6ada233f105
Monster3D <monster3d@mail.ru>
2019-05-01 19:52:23 +0500

    Added new unit test for Settings

--- tests/Unit/Core/SettingsTest.php
+++ tests/Unit/Core/SettingsTest.php
@@ -1,15 +1,77 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: nikolay
- * Date: 01/05/2019
- * Time: 18:21
- */
 
 namespace Tests\Unit\Core;
 
+use GisCalculator\Core\Settings;
+use PHPUnit\Framework\TestCase;
+use ReflectionClass;
+use ReflectionException;
 
-class SettingsTest
+/**
+ * Class SettingsTest
+ * @package Tests\Unit\Core
+ */
+class SettingsTest extends TestCase
 {
+    /**
+     * @return Settings
+     */
+    private function makeSettings() : Settings
+    {
+        return new Settings();
+    }
 
+    /**
+     * @group unit
+     */
+    public function test_getValue_foundValue_returnValue()
+    {
+        //Arrange
+        $settings = $this->makeSettings();
+        try {
+            $reflectionClass = new ReflectionClass($settings);
+            $property = $reflectionClass->getProperty('settingContainer');
+            $property->setAccessible(true);
+            $property->setValue($settings, ['test_key' => 'test_value']);
+        } catch (ReflectionException $e) {
+            $this->markTestSkipped($e->getMessage());
+        }
+
+        //Act
+        $value = $settings->getValue('test_key');
+
+        //Assert
+        $this->assertEquals('test_value', $value);
+    }
+
+    /**
+     * @group unit
+     */
+    public function test_setValue_getValue_foundValue_returnValue()
+    {
+        //Arrange
+        $settings = $this->makeSettings();
+
+        //Act
+        $settings->setValue('test_key', 'test_value');
+        $value = $settings->getValue('test_key');
+
+        //Assert
+        $this->assertEquals('test_value', $value);
+    }
+
+    /**
+     * @group unit
+     */
+    public function test_getValue_notFoundValue_returnNull()
+    {
+        //Arrange
+        $settings = $this->makeSettings();
+
+        //Act
+        $value = $settings->getValue('test_key');
+
+        //Assert
+        $this->assertNull($value);
+    }
 }
