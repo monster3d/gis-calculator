7ab2830b73c88a9ecbb8af32e6b6d91d1de39f1e
Monster3D <monster3d@mail.ru>
2019-05-01 10:48:47 +0500

    Edit unit test

--- tests/Unit/Modules/DistanceTest.php
+++ tests/Unit/Modules/DistanceTest.php
@@ -2,8 +2,8 @@
 
 namespace Tests\Unit\Modules;
 
-
 use GisCalculator\Core\Settings;
+use GisCalculator\Element\Point;
 use GisCalculator\Modules\Distance;
 use PHPUnit\Framework\TestCase;
 
@@ -17,21 +17,37 @@ class DistanceTest extends TestCase
     /**
      * @return Distance
      */
-    private function makeDistace() : Distance
+    private function makeDistance() : Distance
     {
         $defaultSettings = new Settings();
 
         return new Distance($defaultSettings);
     }
 
+    /**
+     * @param float $latitude
+     * @param float $longitude
+     * @return Point
+     */
+    private function makePoint(float $latitude, float $longitude) : Point
+    {
+        return new Point($latitude, $longitude);
+    }
 
     /**
      * @group unit
      */
     public function test_get_calculateDistanceBetweenPoints_returnCorrectDistance()
     {
-        $this->assertTrue(true);
+        //Arrange
+        $distanceModule = $this->makeDistance();
+        $pointA = $this->makePoint(56.836341, 60.621788);
+        $pointB = $this->makePoint(56.827314, 60.625178);
 
+        //Act
+        $distance = $distanceModule->get($pointA, $pointB);
+
+        //Assert
+        $this->assertEquals(1025.8885687402, $distance);
     }
-
 }
