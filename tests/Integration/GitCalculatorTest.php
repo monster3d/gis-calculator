b3a61712ce78d3dd62910577ef1742cfc15f0d6f
Monster3D <monster3d@mail.ru>
2019-05-01 18:08:33 +0500

    Added new integration test

--- tests/Integration/GitCalculatorTest.php
+++ tests/Integration/GitCalculatorTest.php
@@ -1,15 +1,45 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: nikolay
- * Date: 01/05/2019
- * Time: 17:59
- */
 
 namespace Tests\Integration;
 
+use GisCalculator\Element\Point;
+use GisCalculator\GisCalculator;
+use PHPUnit\Framework\TestCase;
 
-class GitCalculatorTest
+/**
+ * Class GitCalculatorTest
+ * @package Tests\Integration
+ */
+class GitCalculatorTest extends TestCase
 {
 
+    /**
+     * @return GisCalculator
+     */
+   private function makeGisCalculator() : GisCalculator
+   {
+       return new GisCalculator();
+   }
+
+   private function makePoint(float $latitude, float $longitude)
+   {
+       return new Point($latitude, $longitude);
+   }
+
+    /**
+     * @group integration
+     */
+   public function test_getDistance_betweenPoints_return_correctDistance()
+   {
+       //Arrange
+       $gisCalculator = $this->makeGisCalculator();
+       $pointA = $this->makePoint(56.836341, 60.621788);
+       $pointB = $this->makePoint(56.827314, 60.625178);
+
+       //Act
+       $distance = $gisCalculator->getDistance($pointA, $pointB);
+
+       //Assert
+       $this->assertEquals(1025.89, $distance);
+   }
 }
