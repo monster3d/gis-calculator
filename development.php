9c2b3fec0314606a11538817e30122278da474ef
Monster3D <monster3d@mail.ru>
2019-05-01 10:49:13 +0500

    Change developemnt data

--- development.php
+++ development.php
@@ -2,11 +2,11 @@
 
 include "vendor/autoload.php";
 
-$lat1 = 56.837744;
-$lat2 = 56.834340;
+$lat1 = 56.836341;
+$lat2 = 56.827314;
 
-$lng1 = 60.633386;
-$lng2 = 60.622657;
+$lng1 = 60.621788;
+$lng2 = 60.625178;
 
 $gitCalculator = new \GisCalculator\GisCalculator();
 
