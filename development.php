703274b832737c74b24f2de5ca7d4c726ca2c072
Monster3D <monster3d@mail.ru>
2019-05-02 10:48:38 +0500

    Added new integration tests

--- development.php
+++ development.php
@@ -22,4 +22,20 @@ foreach ($collection->getIterator() as $p) {
     print_r($p->getLatitude() . PHP_EOL);
 }
 
+$pointCenter = \GisCalculator\GisCalculator::makePoint(56.830794, 60.636087);
+$point1 = \GisCalculator\GisCalculator::makePoint(56.830160, 60.630271);
+$point2 = \GisCalculator\GisCalculator::makePoint(56.826579, 60.637781);
+$point3 = \GisCalculator\GisCalculator::makePoint(56.662034, 60.434503);
+
+$pointCollection = new \GisCalculator\Element\CollectionPoints();
+$pointCollection
+    ->setPoint($point1)
+    ->setPoint($point2)
+    ->setPoint($point3);
+
+$radius = \GisCalculator\GisCalculator::makeRadius(5000000, \GisCalculator\Core\Metric::CENTIMETERS);
+
+$result = $gitCalculator->gisWithCollectionInRadius($pointCenter, $radius, $pointCollection);
+
+print_r($result);
 
