87da0e6fef7a25170e91f7c95c190e894237d956
Monster3D <monster3d@mail.ru>
2019-05-03 12:00:43 +0500

    Added new integration test

--- tests/Integration/GitCalculatorTest.php
+++ tests/Integration/GitCalculatorTest.php
@@ -138,4 +138,43 @@ class GitCalculatorTest extends TestCase
         //Assert
         $this->assertEmpty($result);
     }
+
+    /**
+     * @group integration
+     */
+    public function test_findGisPointIntersection_fountIntersect_returnIntersectPoint()
+    {
+        //Arrange
+        $gisCalculator = $this->makeGisCalculator();
+        $pointA1 = $this->makePoint(56.841919, 60.610369);
+        $pointA2 = $this->makePoint(56.817216, 60.606771);
+        $pointB1 = $this->makePoint(56.831107, 60.567193);
+        $pointB2 = $this->makePoint(56.839423, 60.663087);
+        $intersectPoint = $this->makePoint(56.834760841253, 60.609326411846);
+
+        //Act
+        $intersect = $gisCalculator->findGisPointIntersection($pointA1, $pointA2, $pointB1, $pointB2);
+
+        //Assert
+        $this->assertEquals($intersectPoint, $intersect);
+    }
+
+    /**
+     * @group integration
+     */
+    public function test_findGisPointIntersection_notFountIntersect_returnNull()
+    {
+        //Arrange
+        $gisCalculator = $this->makeGisCalculator();
+        $pointA1 = $this->makePoint(56.848096, 60.553014);
+        $pointA2 = $this->makePoint(56.811283, 60.552348);
+        $pointB1 = $this->makePoint(56.855400, 60.651259);
+        $pointB2 = $this->makePoint(56.816036, 60.670742);
+
+        //Act
+        $intersect = $gisCalculator->findGisPointIntersection($pointA1, $pointA2, $pointB1, $pointB2);
+
+        //Assert
+        $this->assertNull($intersect);
+    }
 }
